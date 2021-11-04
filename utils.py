0421a656f1acd3a1939cb1366eaadfcd666f0125
Константин Ушаков <ushk2010ushk@gmail.com>
2021-11-04 19:16:00 +0600

    Add comparable with modern libraries

--- utils.py
+++ utils.py
@@ -4,13 +4,57 @@ import numpy as np
 
 from os import listdir, mkdir, sep
 from os.path import join, exists, splitext
-from scipy.misc import imread, imsave, imresize
+# from scipy.misc import imread, imsave, imresize
+# ImportError: cannot import name 'imread' from 'scipy.misc'
+# https://github.com/titu1994/Neural-Style-Transfer/issues/59#issuecomment-515782686
 import skimage
 import skimage.io
 import skimage.transform
 import tensorflow as tf
 from PIL import Image
 from functools import reduce
+
+import numpy as np
+import imageio
+from PIL import Image
+from skimage import color
+
+
+def imread(path, mode="RGB"):
+    # Loads data in HxW format, then transposes to correct format
+    img = np.array(imageio.imread(path, pilmode=mode))
+    return img
+
+
+def imresize(img, size, interp='bilinear'):
+    """
+    Resizes an image
+    :param img:
+    :param size: (Must be H, W format !)
+    :param interp:
+    :return:
+    """
+    if interp == 'bilinear':
+        interpolation = Image.BILINEAR
+    elif interp == 'bicubic':
+        interpolation = Image.BICUBIC
+    else:
+        interpolation = Image.NEAREST
+
+    # Requires size to be HxW
+    size = (size[1], size[0])
+
+    if type(img) != Image:
+        img = Image.fromarray(img, mode='RGB')
+
+    img = np.array(img.resize(size, interpolation))
+    return img
+
+
+def imsave(path, img):
+    imageio.imwrite(path, img)
+    return
+
 
 def list_images(directory):
     images = []
@@ -25,7 +69,7 @@ def list_images(directory):
     return images
 
 
-def get_train_images(paths, resize_len=512, crop_height=256, crop_width=256, flag = True):
+def get_train_images(paths, resize_len=512, crop_height=256, crop_width=256, flag=True):
     if isinstance(paths, str):
         paths = [paths]
 
@@ -70,7 +114,7 @@ def save_images(paths, datas, save_path, prefix=None, suffix=None):
     if isinstance(paths, str):
         paths = [paths]
 
-    assert(len(paths) == len(datas))
+    assert (len(paths) == len(datas))
 
     if not exists(save_path):
         mkdir(save_path)
@@ -88,15 +132,15 @@ def save_images(paths, datas, save_path, prefix=None, suffix=None):
 
         name, ext = splitext(path)
         name = name.split(sep)[-1]
-        
+
         path = join(save_path, prefix + suffix + ext)
         print('data path==>>', path)
-
 
         # new_im = Image.fromarray(data)
         # new_im.show()
 
         imsave(path, data)
+
 
 def get_l2_norm_loss(diffs):
     shape = diffs.get_shape().as_list()
